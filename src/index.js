44993f08e2b776e0181fdaf4584c9d2e2ab84eae
Sanket Singh <singhsanket143@gmail.com>
2024-06-03 20:52:18 +0530

    Tested userschema

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const ServerConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
+// const User = require('./schema/userSchema');
 
 const app = express();
 
@@ -17,4 +18,15 @@ app.post('/ping', (req, res) => {
 app.listen(ServerConfig.PORT, async () => {
     await connectDB();
     console.log(`Server started at port ${ServerConfig.PORT}...!!`);
+
+    // const newUser = await User.create({
+    //     email: 'a@b.com',
+    //     password: '123456',
+    //     firstName: 'Jonathan',
+    //     lastName: 'Majors',
+    //     mobileNumber: '1231231230'
+    // });
+
+    // console.log("Created new user");
+    // console.log(newUser);
 });
