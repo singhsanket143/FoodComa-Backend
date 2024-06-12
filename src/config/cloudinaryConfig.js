e58fba091e4569ab3e30d50b2402fa41fd654a7e
Sanket Singh <singhsanket143@gmail.com>
2024-06-12 21:41:37 +0530

    Added cloudinary config

---
+++ src/config/cloudinaryConfig.js
@@ -0,0 +1,12 @@
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = require('./serverConfig');
+
+const cloudinary = require('cloudinary').v2;
+
+// configuring cloudinary
+cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+});
+
+module.exports = cloudinary;
--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -6,5 +6,8 @@ module.exports = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
     JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRY: process.env.JWT_EXPIRY
+    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME
 }
