933a64de6dfd5574d7661cafc9a03c3bfe4ed75d
Sanket Singh <singhsanket143@gmail.com>
2024-06-14 21:23:46 +0530

    Added cart model

---
+++ src/schema/cartSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                default: 1
+            }
+        }
+    ],
+}, {
+    timestamps: true
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
