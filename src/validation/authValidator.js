f64d42566a9a52f712700faed7e56f7d9b7aef95
Sanket Singh <singhsanket143@gmail.com>
2024-06-30 22:17:10 +0530

    Added code changes

--- src/validation/authValidator.js
+++ src/validation/authValidator.js
@@ -4,6 +4,7 @@ const UnAuthorisedError = require('../utils/unauthorisedError');
 
 async function isLoggedIn(req, res, next) {
     const token = req.cookies["authToken"];
+    console.log(token);
     if(!token) {
         return res.status(401).json({
             success: false,
@@ -15,7 +16,8 @@ async function isLoggedIn(req, res, next) {
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
-        
+        console.log(decoded, decoded.exp, Date.now() / 1000);
+
         if(!decoded) {
             throw new UnAuthorisedError();
         }
@@ -29,6 +31,20 @@ async function isLoggedIn(req, res, next) {
 
         next();
     } catch (error) {
+        console.log(error.name);
+        if(error.name === "TokenExpiredError") {
+            res.cookie("authToken", "", {
+                httpOnly: true,
+                secure: false,
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            });
+            return res.status(200).json({
+                success: true,
+                message: "Log out successfull",
+                error: {},
+                data: {}
+            });
+        }
         return res.status(401).json({
             success: false,
             data: {},
